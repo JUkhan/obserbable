0dd6ed65c3d0d13366b9eb2339b537420b2420fd
Jasim Khan <jasim.uddin.khan@gmail.com>
2019-03-16 06:12:47 +0600

    create app.js

---
+++ app.js
@@ -0,0 +1,139 @@
+
+function range({ start = 0, end, step = 1 }) {
+    return {
+        [Symbol.iterator]() {
+            return {
+                next() {
+                    if (start <= end) {
+                        const obj = { value: start, done: false }
+                        start += step
+                        return obj
+                    }
+                    return { done: true }
+                }
+            }
+
+        }
+    }
+}
+
+function map(selector) {
+    return (source) => {
+        return new Observable(destination => {
+            return source.subscribe({
+                next(res) {
+                    destination.next(selector(res))
+                },
+                complete() {
+                    destination.complete && destination.complete();
+                }
+            })
+
+        })
+    }
+}
+
+function filter(selector) {
+    return (source) => {
+        return new Observable(destination => {
+            return source.subscribe({
+                next(res) {
+
+                    selector(res) && destination.next(res)
+                },
+                complete() {
+                    destination.complete();
+                }
+            })
+
+        })
+    }
+}
+
+function take(limit) {
+    return (source) => {
+        return new Observable(destination => {
+            let count = 0;
+            return source.subscribe({
+                next(val) {
+                    if (count < limit) {
+                        count++;
+                        destination.next(val)
+                    } else {
+                        destination.isDone = true;
+                        destination.unsubscribe();
+                    }
+                },
+                complete() {
+                    destination.complete();
+                }
+            })
+        });
+    }
+}
+
+class Observable {
+    constructor(subscribe) {
+        this.__subscribe = subscribe;
+        this.isUnsubscribe = false;
+    }
+    subscribe(observer) {
+        const next = observer.next;
+        const complete = observer.complete || function () { };
+        observer.isDone = false;
+        observer.next = function (val) {
+            !observer.isDone && next(val)
+        }
+        observer.complete = function () {
+            observer.isDone = true;
+            complete()
+        }
+        let subscription;
+        observer.unsubscribe = function () {
+            observer.isDone = false;
+            return typeof subscription === 'function' ? subscription() : subscription;
+        }
+        subscription = this.__subscribe(observer);
+        return observer.unsubscribe;
+    }
+
+    pipe(...operators) {
+        return operators.reduce((source, operator) => operator(source), this);
+    }
+    
+    static of(...args) {
+        return new Observable(observer => {
+            args.forEach(observer.next);
+            observer.complete();
+            return 1010
+        })
+    }
+    
+    static interval(millisecond) {
+        return new Observable(destination => {
+            let i = 1, tid = setInterval(() => {
+                destination.next(i++);
+            }, millisecond);
+            return () => clearInterval(tid);
+        });
+
+    }
+}
+
+
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).pipe(
+    map(v => v * 3),
+    take(5)
+).subscribe({
+    next(val) {
+        console.log(val)
+    }
+});
+
+var subs = Observable.interval(100).pipe(
+    take(5)
+).subscribe({
+    next(val) { console.log(val) }
+});
+
+
